ed2a43ba8be07040e90f4657d86bb7615d9b60df
조현지 <qndjddl001@gmail.com>
2024-10-02 14:44:19 +0900

    fix: firebase 오타 수정

--- src/js/firebaseSetting.js
+++ src/js/firebaseSetting.js
@@ -1,11 +1,11 @@
 // firebase setting
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import {getFirestore} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {collection, addDoc} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {getDocs} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyDBzg-CgJ_rurOKVL3QsX14yfRyat4fy6c",
     authDomain: "world-6960c.firebaseapp.com",
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // firebase.js에 있는 함수, 변수 등을 외부 페이지에서 사용하기 위한 export
-export {db, app, getDoc, collection, addDoc};
+export {db, getDocs, collection, addDoc };
